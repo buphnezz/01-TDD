2405e05d3003babd0180d121bb741a1d96049672
zachary schumpert <buphnezz@gmail.com>
2018-04-27 17:18:56 -0700

    lab-01-TDD-RESUBMIT

--- lab-zachary/src/__test__/greet.test.js
+++ lab-zachary/src/__test__/greet.test.js
@@ -6,14 +6,9 @@ describe('greet.test.js', () => {
   describe('greet.hi', () => {
     test('should return Null when no string', () => {
       expect(greet.hi(null)).toBeNull();
-    })
-  })
-})
-
-describe('greet.test.js', () => {
-  describe('greet.hi', () => {
+    });
     test('Should return hello world', () => {
       expect(greet.hi('world')).toEqual('Hello, world!');
-    })
-  })
+    });
+  });
 });
--- lab-zachary/src/lib/arithmetic.js
+++ lab-zachary/src/lib/arithmetic.js
@@ -3,8 +3,22 @@
 const arithmetic = module.exports = {};
 
 arithmetic.add = (num1, num2) => {
-  if (num1 || num2 === '' || typeof (num1, num2) !== number) {
+  if (num1 === '' || num2 === '' || typeof num1 !== 'number' || typeof num2 !== 'number') {
     return null;
-  };
-  return `Hello, ${userInput}!`;
+  }
+  return 'There is no number present';
+};
+
+arithmetic.sub = (num1, num2) => {
+  if (num1 === '' || num2 === '' || typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return null;
+  }
+  return num1 - num2;
+};
+
+arithmetic.numIsPresent = (num1, num2) => {
+  if (typeof num1 === 'number' || typeof num2 === 'number') {
+    return 2;
+  }
+  return 2;
 };
